a9fdcb576e371d9b225a31520f0e4ba713b10b14
irrwahn <irrwahn35@freenet.de>
2016-08-12 13:46:35 +0000

    * Improved event driven GUI update to suppress list flickering. * Small fixes and improvements.

--- pygui/frelay.py
+++ pygui/frelay.py
@@ -75,8 +75,7 @@ Status.grid(row=4, columnspan=2, sticky=NW+SE)
 
 # Triggered to process client data
 def proc_clt(event=None):
-    subproc_clt()
-    return False
+    root.after(0, subproc_clt) # Not called directly to avoid flickering!
 
 root.bind('<<cltdata>>', proc_clt)
 
@@ -99,7 +98,7 @@ def id2name(peerid):
 
 def logadd(line):
     log.config(state=NORMAL)
-    log.insert(END, time.strftime('%H:%M:%S ') + line + "\n")
+    log.insert(END, time.strftime('%H:%M:%S ') + line.expandtabs(8) + "\n")
     log.config(state=DISABLED)
 
 
@@ -128,93 +127,93 @@ def clt_write(cmd):
         proc.stdin.flush()
 
 # Process queued client output
-# Called from proc_clt(), which is bound to the <<cltdata>> virtual event
+# Called via root.after() from proc_clt(), which in turn
+# is bound to the <<cltdata>> virtual event. Sigh.
 def subproc_clt():
     global connected
     global authed
-    while True :
-        try:  b = readq.get_nowait()
-        except Empty:
-            break
+    try:  b = readq.get_nowait()
+    except Empty:
+        return
+    else:
+        logscrl = True
+        line = b.decode(encoding='utf-8').strip()
+        if len(line) > 4 and line[4] == ':' and line[:4].isupper():
+            pfx = line[:4]
+            line = line[5:]
         else:
-            logscrl = True
-            line = b.decode(encoding='utf-8').strip()
-            if len(line) > 4 and line[4] == ':' and line[:4].isupper():
-                pfx = line[:4]
-                line = line[5:]
+            pfx = ''
+    # Triage lines based on prefix
+    # Connection and login status
+        if pfx == 'CONN':
+            authed = False
+            connected = True
+            Status.config(bg=scol_conn, text=line)
+            logadd(line)
+        elif pfx == 'DISC':
+            authed = False
+            connected = False
+            Status.config(bg=scol_disc, text=line)
+            logadd(line)
+        elif pfx == 'AUTH':
+            authed = True
+            Status.config(bg=scol_auth, text=line)
+            logadd(line)
+        elif pfx == 'NAUT':
+            authed = False
+            Status.config(bg=scol_conn, text=line)
+            logadd(line)
+    # Pings
+        elif pfx == 'QPNG':
+            cidx = line.find(':')
+            if cidx != -1:
+                peername=id2name(line[:cidx][-16:])
+                logadd("[" + peername + "] " + line[cidx+2:].strip("'"))
+        elif pfx == 'RPNG':
+            cidx = line.find(':')
+            if cidx != -1:
+                peername=id2name(line[:cidx][-16:])
+                logadd("<" + peername + "> " + line[cidx+2:])
+    # Server messages
+        elif pfx == 'SMSG':
+            logadd('[' + srvalias + '] ' + line)
+    # Informational messages
+        elif pfx == 'IMSG':
+            logadd(line)
+    # External command output
+        elif pfx == 'COUT':
+            logadd(line)
+    # Peer list item
+        elif pfx == 'PLST':
+            line = line.lstrip('0')
+            if not line:
+                peerlist.delete(0, END)
             else:
-                pfx = ''
-        # Triage lines based on prefix
-        # Connection and login status
-            if pfx == 'CONN':
-                authed = False
-                connected = True
-                Status.config(bg=scol_conn, text=line)
-                logadd(line)
-            elif pfx == 'DISC':
-                authed = False
-                connected = False
-                Status.config(bg=scol_disc, text=line)
-                logadd(line)
-            elif pfx == 'AUTH':
-                authed = True
-                Status.config(bg=scol_auth, text=line)
-                logadd(line)
-            elif pfx == 'NAUT':
-                authed = False
-                Status.config(bg=scol_conn, text=line)
-                logadd(line)
-        # Pings
-            elif pfx == 'QPNG':
-                cidx = line.find(':')
-                if cidx != -1:
-                    peername=id2name(line[:cidx][-16:])
-                    logadd("[" + peername + "] " + line[cidx+2:].strip("'"))
-            elif pfx == 'RPNG':
-                cidx = line.find(':')
-                if cidx != -1:
-                    peername=id2name(line[:cidx][-16:])
-                    logadd("<" + peername + "> " + line[cidx+2:])
-        # Server messages
-            elif pfx == 'SMSG':
+                peerlist.insert(END, line)
+            logscrl = False
+    # Transfer list item
+        elif pfx == 'TLST':
+            if not line:
+                translist.delete(0, END)
+            else:
+                translist.insert(END, line)
+            logscrl = False
+        elif pfx == 'OFFR':
+            logadd("ToDo offr: " + line)
+        elif pfx == 'CERR':
+            logadd("ToDo cerr: " + line)
+        elif pfx == 'LERR':
+            logadd("ToDo lerr: " + line)
+        elif pfx == 'SERR':
+            logadd("ToDo serr: " + line)
+    # Unhandled
+        else:
+            if not pfx: # MotD hack!
                 logadd('[' + srvalias + '] ' + line)
-        # Informational messages
-            elif pfx == 'IMSG':
-                logadd(line)
-        # External command output
-            elif pfx == 'COUT':
-                logadd(line)
-        # Peer list item
-            elif pfx == 'PLST':
-                line = line.lstrip('0')
-                if not line:
-                    peerlist.delete(0, END)
-                else:
-                    peerlist.insert(END, line)
-                logscrl = False
-        # Transfer list item
-            elif pfx == 'TLST':
-                if not line:
-                    translist.delete(0, END)
-                else:
-                    translist.insert(END, line)
-                logscrl = False
-            elif pfx == 'OFFR':
-                logadd("ToDo offr: " + line)
-            elif pfx == 'CERR':
-                logadd("ToDo cerr: " + line)
-            elif pfx == 'LERR':
-                logadd("ToDo lerr: " + line)
-            elif pfx == 'SERR':
-                logadd("ToDo serr: " + line)
-        # Unhandled
             else:
-                if not pfx: # MotD hack!
-                    logadd('[' + srvalias + '] ' + line)
-                else:
-                    logadd(pfx + ':' + line)
-            if logscrl:
-                log.see("end")
+                logadd(pfx + ':' + line)
+        if logscrl:
+            log.see("end")
 
 # Scheduled list refreshing
 def subrefresh_local():
@@ -222,18 +221,18 @@ def subrefresh_local():
         clt_write("list");
     else:
         translist.delete(0,END)
-    root.after(refresh_local_ms, subrefresh_local);
+    root.after(refresh_local_ms, subrefresh_local)
 
 def subrefresh_remote():
     if authed:
         clt_write("peerlist");
     else:
         peerlist.delete(0, END)
-    root.after(refresh_remote_ms, subrefresh_remote);
+    root.after(refresh_remote_ms, subrefresh_remote)
 
 # Main loop
-root.after(0, subrefresh_local);
-root.after(0, subrefresh_remote);
+root.after(0, subrefresh_local)
+root.after(0, subrefresh_remote)
 root.mainloop()
 
 
