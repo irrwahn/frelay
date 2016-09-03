bc25350db019776c5eed0c696df063e1eca1f7e2
irrwahn <irrwahn35@freenet.de>
2016-09-03 11:32:02 +0000

    * Converted SVGs from Inkscape format to plain SVG. * Use rsvg-convert for SVG to PNG conversion (instead of inkscape).

--- pygui/icon_src/svg2py.sh
+++ pygui/icon_src/svg2py.sh
@@ -8,8 +8,12 @@ of=icons.py
 rm -f $of
 
 for s in *.svg ; do
+    echo "$s"
     f="${s%.*}"
-    inkscape -z -e "$f.png" -w16 -h16 "$s"
+
+# Inkscape can be used as an alternative to rsvg-convert.
+#    inkscape -z -e "$f.png" -w16 -h16 "$s"
+    rsvg-convert -w 16 -h 16 -f png -o "$f.png" "$s"
 
     echo "${f}_png = '''" >> $of
     base64 "$f.png" >> $of
